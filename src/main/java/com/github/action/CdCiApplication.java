aac237352ef07e32114caded6b0ed1d10945d479
teyyub <teyyub392@gmail.com>
2024-01-26 01:43:29 +0400

    second

---
+++ src/main/java/com/github/action/CdCiApplication.java
@@ -0,0 +1,13 @@
+package com.github.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdCiApplication.class, args);
+	}
+
+}
